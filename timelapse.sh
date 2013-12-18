24f4013be2873c8021ff29ee858e21aacae10f16
Rafael Biriba <biribarj@gmail.com>
2013-12-18 10:48:52 -0200

    script para fazer video timelapse no ffmpeg

---
+++ timelapse.sh
@@ -0,0 +1,5 @@
+ffmpeg -i video-rapidofull-video-quad-h-build.mov -vf setpts=0.00333*PTS -an video-rapido.mp4
+
+minuto do video final / minuto do video
+
+1 minuto timelapse / 40 minutos de video = 0.022 * PTS
