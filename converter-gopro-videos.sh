6a284b0b1df5c0ec34cf0763b20cc6d793419b8e
Rafael Biriba <biribarj@gmail.com>
2013-12-20 20:24:59 -0200

    aumentando o bitrate da conversao de videos da gopro

--- converter-gopro-videos.sh
+++ converter-gopro-videos.sh
@@ -17,6 +17,6 @@ do
   ORIGINAL=`basename "$file" .mp4`
   NEWNAME=${ORIGINAL}.mp4
 
- ffmpeg -y -i "$file" -r 30000/1001 -threads 4 -b:v 3M -bt 4M -vcodec libx264 -filter:v yadif -pass 1 -preset faster -an "$DIR/converted/$NEWNAME" </dev/null
- ffmpeg -y -i "$file" -r 30000/1001 -threads 4 -b:v 3M -bt 4M -vcodec libx264 -filter:v yadif -pass 2 -preset faster -acodec libfaac -ac 2 -ar 48000 -ab 192k "$DIR/converted/$NEWNAME"  </dev/null
+ ffmpeg -y -i "$file" -r 30000/1001 -threads 4 -b:v 4500K -bt 5000K -vcodec libx264 -filter:v yadif -pass 1 -preset fast -an "$DIR/converted/$NEWNAME" </dev/null
+ ffmpeg -y -i "$file" -r 30000/1001 -threads 4 -b:v 4500K -bt 5000K -vcodec libx264 -filter:v yadif -pass 2 -preset fast -acodec libfaac -ac 2 -ar 48000 -ab 192k "$DIR/converted/$NEWNAME"  </dev/null
 done < <(find $start \( -iname \*.mp4 \))
